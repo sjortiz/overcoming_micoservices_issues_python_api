620b3e904b1f3c81cf5202ca2c0b3c2ed4d06761
ShailynOrtiz <sortiz@intellisys.com.do>
2019-09-26 02:24:21 -0400

    adding the cache wraper to the get_with_id function to not hit the db in each requests

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-import time
 from pymongo import MongoClient
 from flask import Flask, request
 from flask_restful import Resource, Api
 from bson.objectid import ObjectId
+from redis import StrictRedis
+from redis_cache import RedisCache
 
 # Flask configuration
 app = Flask(__name__)
@@ -16,9 +17,13 @@ client = MongoClient(
         )
 db = client['todos']
 collection = db['tasks']
+# Redis cache configuraiton
+client = StrictRedis(host="0.0.0.0", port=6379, db=0, decode_responses=True)
+cache = RedisCache(redis_client=client)
 
 
 # Separating the get_with_id function from the endpoint
+@cache.cache(ttl=5)
 def get_with_id(todo_id):
 
     data = collection.find_one({'_id': ObjectId(todo_id)})
