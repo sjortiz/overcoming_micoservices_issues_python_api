b8433d62b3436f259e2c7b8508a17b2221d78e1b
ShailynOrtiz <sortiz@intellisys.com.do>
2019-09-25 21:58:53 -0400

    simulating todo -- Stateful

--- api.py
+++ api.py
@@ -4,11 +4,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-            return {'hello': 'world'}
+tasks = []
 
-api.add_resource(HelloWorld, '/')
+class Todo(Resource):
+    def get(self):
+            return tasks
+    
+    def post(self):
+        tasks.append({'task': len(tasks)+1})
+
+api.add_resource(Todo, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
