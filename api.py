037ee94021b2f8cf724d6ddd9a19e3ba3158b0be
ShailynOrtiz <sortiz@intellisys.com.do>
2019-09-26 01:50:43 -0400

    turning the app into stateless app by moving the state to a database

--- api.py
+++ api.py
@@ -1,31 +1,57 @@
-from flask import Flask
+from pymongo import MongoClient
+from flask import Flask, request
 from flask_restful import Resource, Api
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 api = Api(app)
-tasks = []
+client = MongoClient(
+            host="0.0.0.0",
+            port=27017,
+            username="root",
+            password="toor",
+        )
+db = client['todos']
+collection = db['tasks']
 
 
 class Todos(Resource):
-    def get(self, todo_id=None):
-        return tasks
+    def get(self):
+        return [
+            {
+                **task,
+                **{'_id': str(task['_id'])}
+            }
+            for task in collection.find({})
+        ]
 
     def post(self):
-        tasks.append({'task': len(tasks)+1})
+
+        data = request.get_json().get('task')
+
+        if data:
+            task_id = collection.insert_one({
+                'task': data
+            })
+
+            return {'ID': str(task_id.inserted_id)}
+
+        return {}
 
 
 class TodoItem(Resource):
 
     def get(self, todo_id):
-        for x in tasks:
-            if x['task'] == todo_id:
-                return x
+        data = collection.find_one({'_id': ObjectId(todo_id)})
 
-        return {}
+        if data:
+            data.update({'_id': str(data['_id'])})
+
+        return data or {}
 
 
 api.add_resource(Todos, '/')
-api.add_resource(TodoItem, '/id/<int:todo_id>')
+api.add_resource(TodoItem, '/id/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
